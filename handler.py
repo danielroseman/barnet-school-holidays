7fc79980de0fb2eb6350321c1d988bc4fb896461
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2017-08-07 21:39:54 +0100

    Refactor handler for more request types and intents

--- handler.py
+++ handler.py
@@ -2,31 +2,74 @@ import dates
 
 import datetime
 import json
+import logging
 
-def get_event(event, context):
-  text = None
-  slots = event['request']['intent']['slots']
-  if slots:
-    datestr = slots['Date']['value']
-    try:
-      date = datetime.datetime.strptime(datestr, '%Y-%m-%d').date()
-    except ValueError:
-      text = "Sorry, I didn't understand that date"
-  else:
-    date = None
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-  if not text:
-    next_event = dates.find_next(date)
 
+
+def make_response(text, card_title='Thanks', should_end_session=False,
+                  reprompt_text=None):
   response = {
     'version': '1.0',
     'response': {
       'outputSpeech': {
         'type': 'PlainText',
-        'text': next_event,
-      }
+        'text': text,
+      },
+      'card': {
+        'type': 'Simple',
+        'title': card_title,
+        'content': text
+      },
+      'shouldEndSession': should_end_session
     }
   }
+  if reprompt_text:
+    response['reprompt'] = {
+      'outputSpeech': {
+        'type': 'PlainText',
+        'text': reprompt_text
+      }
+    }
 
   return response
+
+
+def dispatch(event, context):
+  request = event['request']
+
+  if request['type'] == 'LaunchRequest':
+    return make_response(get_event({}))
+  elif request['type'] == 'IntentRequest':
+    intent = request['intent']
+    if intent['name'] == 'NextHoliday':
+      return make_response(get_event(intent))
+    elif intent['name'] == 'AMAZON.HelpIntent':
+      text = ('Ask me for the next school holiday, or for the next holiday '
+              'after a certain date.')
+      return make_response(text, reprompt_text=text)
+    elif intent['name'] in ('AMAZON.StopIntent', 'AMAZON.CancelIntent'):
+      return make_response(
+          'Thank you for using Barnet School Holidays',
+          card_title='Goodbye',
+          should_end_session=True
+      )
+
+  return make_response("Sorry, I didn't understand that request.")
+
+
+def get_event(intent):
+  date = None
+  slots = intent.get('slots')
+  if slots:
+    datestr = slots['Date'].get('value')
+    if datestr:
+      try:
+        date = datetime.datetime.strptime(datestr, '%Y-%m-%d').date()
+      except ValueError:
+        return "Sorry, I didn't understand that date"
+
+  return dates.find_next(date)
 
